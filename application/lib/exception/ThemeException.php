3be30416b41fcf3fc41f66f1e23e0c620a01b15b
251328109 <251328109@qq.com>
2018-02-01 20:56:55 +0800

    jie

--- application/lib/exception/ThemeException.php
+++ application/lib/exception/ThemeException.php
@@ -11,5 +11,7 @@ namespace app\lib\exception;
 
 class ThemeException extends BaseException
 {
-    
+    public $code = 404;
+    public $msg = '指定主题不存在,请检查主题ID';
+    public $errorCode = 30000;
 }
