52ebcb480d5fce712394390d47c207bb1a5bc94c
pythonic <936968629@qq.com>
2018-02-02 17:32:55 +0800

    wen

--- application/api/controller/v1/Theme.php
+++ application/api/controller/v1/Theme.php
@@ -8,12 +8,23 @@
 
 namespace app\api\controller\v1;
 
+use app\api\controller\BaseController;
+use app\api\validate\IDCollection;
+use app\api\model\Theme as ThemeModel;
+use app\lib\exception\ThemeException;
 
-use think\Controller;
-
-class Theme extends Controller
+class Theme extends BaseController
 {
-    public function getTheme($ids){
-        
+
+    public function getSimpleList($ids=''){
+        ( new IDCollection() )->goCheck();
+        $ids = explode(',',$ids);
+        $result = ThemeModel::all($ids);
+        return $result;
+        if( $result->isEmpty() ){
+            throw new ThemeException([]);
+        }
+        return $result;
+
     }
 }
