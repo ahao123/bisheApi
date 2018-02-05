9725310ee918a4ad743f082aeb83f8a9c1cd735b
251328109 <251328109@qq.com>
2018-02-05 20:46:36 +0800

    w

--- application/api/controller/v1/Theme.php
+++ application/api/controller/v1/Theme.php
@@ -11,18 +11,17 @@ namespace app\api\controller\v1;
 use app\api\controller\BaseController;
 use app\api\validate\IDCollection;
 use app\api\model\Theme as ThemeModel;
-use app\lib\exception\ThemeException;
+use app\lib\exception\DataException;
 
 class Theme extends BaseController
 {
-
     public function getSimpleList($ids=''){
         ( new IDCollection() )->goCheck();
         $ids = explode(',',$ids);
         $result = ThemeModel::all($ids);
-        return $result;
-        if( $result->isEmpty() ){
-            throw new ThemeException([]);
+
+        if( !$result ){
+            throw new DataException([]);
         }
         return $result;
 
