b807e3c64fe3145116eb15f76e928520a19a6ca7
pythonic <936968629@qq.com>
2018-02-08 17:54:51 +0800

    w

--- application/api/controller/v2/Upload.php
+++ application/api/controller/v2/Upload.php
@@ -12,12 +12,11 @@ namespace app\api\controller\v2;
 class Upload
 {
     public function upload(){
-        $params = $_POST['file'];
-        $files = json_decode($params,true);
-
-
-        move_uploaded_file($files['tmp_name'],'../uploads/'.$files['name']);
-        echo "success";
-
+//        $params = $_POST['file'];
+//        $files = json_decode($params,true);
+//        move_uploaded_file($files['tmp_name'],'../uploads/'.$files['name']);
+//        echo "success";
+        $files = $_FILES['file_head'];
+        move_uploaded_file($_FILES['file_head']['tmp_name'],'./upload/'.$_FILES['file_head']['name']);
     }
 }
