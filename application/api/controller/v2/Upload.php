a2395a26d8536c1ba6cad46c76da708b10133085
pythonic <936968629@qq.com>
2018-02-08 15:48:26 +0800

    wen

---
+++ application/api/controller/v2/Upload.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/2/7
+ * Time: 15:53
+ */
+
+namespace app\api\controller\v2;
+
+
+class Upload
+{
+    public function upload(){
+
+        echo 2;
+
+    }
+}
