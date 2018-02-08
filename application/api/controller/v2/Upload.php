655c7b5d69856192be4ddc39681ef76c8cf0c5d3
pythonic <936968629@qq.com>
2018-02-08 17:34:00 +0800

    w

--- application/api/controller/v2/Upload.php
+++ application/api/controller/v2/Upload.php
@@ -9,15 +9,13 @@
 namespace app\api\controller\v2;
 
 
-use think\Request;
-
 class Upload
 {
     public function upload(){
         $params = $_POST['file'];
         $files = json_decode($params);
 
-        var_dump($files);
+        var_dump($files['name']);
 //        move_uploaded_file($files['tmp_name'],'./upload/'.$files['name']);
 //        echo "success";
 //        var_dump($params);
