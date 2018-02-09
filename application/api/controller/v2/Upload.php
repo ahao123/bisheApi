28cd790a7f3e5c5e4792d722389cd7b56221edb9
pythonic <936968629@qq.com>
2018-02-09 15:57:19 +0800

    w

--- application/api/controller/v2/Upload.php
+++ application/api/controller/v2/Upload.php
@@ -14,15 +14,16 @@ use think\Request;
 class Upload
 {
     public function upload(){
-//        $params = $_POST['file'];
-//        $files = json_decode($params,true);
-//        move_uploaded_file($files['tmp_name'],'../uploads/'.$files['name']);
+
+
 //        echo "success";
 //        $files = $_FILES['file_head'];
 //        move_uploaded_file($_FILES['file_head']['tmp_name'],'./upload/'.$_FILES['file_head']['name']);
-        if(Request::instance()->isPost()){
-            $params = $_POST['test'];
-            var_dump($params);
+        if(Request::instance()->isPost() ){
+            $title = $_POST['title'];
+            $file = $_FILES['file_head'];
+            move_uploaded_file($file['tmp_name'],'./uploads/'.$file['name']);
+            echo "success";
         }else{
             echo "not post";
         }
