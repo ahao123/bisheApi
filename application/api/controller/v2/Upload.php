a161ec51707b99c6f646382f556fd97d8087b2a9
pythonic <936968629@qq.com>
2018-02-09 17:33:27 +0800

    w

--- application/api/controller/v2/Upload.php
+++ application/api/controller/v2/Upload.php
@@ -8,16 +8,34 @@
 
 namespace app\api\controller\v2;
 
-
 use think\Request;
+use app\api\model\Theme as ThemeModel;
 
 class Upload
 {
     public function upload(){
         if(Request::instance()->isPost() ){
-            $title = $_POST['title'];
+            $id = $_POST['id'];
             $file = $_FILES['file_head'];
-            move_uploaded_file($file['tmp_name'],'./upload/image/'.$file['name']);
+            $filePath = './upload/image/';
+            $date = date('Ymd');
+
+            if(!is_dir($filePath.$date)){
+                mkdir($filePath.$date);
+            }
+//            list($usec, $sec) = explode(" ", microtime());
+            $fileName = md5($file['name']);
+
+            //插入数据库
+            $filePath2 = $date."/".$fileName;
+            $info = ThemeModel::get($id);
+            if(!$info){
+                return "error";
+            }
+            $info->head_img = $filePath2;
+            $info->save();
+            move_uploaded_file($file['tmp_name'],$filePath.$date."/".$fileName);
+
             echo "success";
         }else{
             echo "not post";
