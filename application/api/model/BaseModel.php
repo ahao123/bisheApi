faa57f4a5741f01fb66a18ed5eed91c3d0137fe2
251328109 <251328109@qq.com>
2018-02-20 23:29:28 +0800

    wen

--- application/api/model/BaseModel.php
+++ application/api/model/BaseModel.php
@@ -13,11 +13,11 @@ use think\Model;
 class BaseModel extends Model
 {
     //读取器 构件image url路径
-//    public function prefixImgUrl($value,$data){
-//        $finnal_url = $value;
-//        if($data['from'] == 1){//本地图片
-//            $finnal_url = config('setting.img_prefix').$value;
-//        }
-//        return $finnal_url;
-//    }
+    public function prefixImgUrl($value,$data){
+        $finnal_url = $value;
+        if($data['from'] == 1){//本地图片
+            $finnal_url = config('setting.img_prefix').$value;
+        }
+        return $finnal_url;
+    }
 }
