824b8468519e211a9b8d80a64cd635e9a6000c79
251328109 <251328109@qq.com>
2018-02-23 22:28:19 +0800

    wen
    order

---
+++ application/api/model/OrderProduct.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/8/16 0016
+ * Time: 23:13
+ */
+
+namespace app\api\model;
+
+
+class OrderProduct extends BaseModel
+{
+
+}
