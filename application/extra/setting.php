59c0fe2d4c01d4fdd35571c3c09e5e41cf8ed077
251328109 <251328109@qq.com>
2018-03-11 13:17:58 +0800

    q

--- application/extra/setting.php
+++ application/extra/setting.php
@@ -7,7 +7,7 @@
  * Time: 22:19
  */
 return [
-    'img_prefix' => 'http://www.wenjunlin.xyz/images',
+    'img_prefix' => 'http://www.wenjunlin.xyz',
 //    'img_prefix' => 'http://bisheapi.project.com/images',
     'token_expire' => '7200'
 ];
